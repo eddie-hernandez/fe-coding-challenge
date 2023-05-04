fd469827679461eced58e711f3507eeff19d37e2
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-05-04 15:28:39 -0400

    setting up custom styling in global and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        navbar: '#729873',
+        btnbg: '#85cd91',
+        btnhover: '#c8f9d0'
+      },
+      fontFamily: {
+        body: ['Urbanist']
+      }
+    },
   },
   plugins: [],
 }
