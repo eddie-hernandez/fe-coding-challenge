3607dfa850ac67c06f8c68adc3498920f23b831b
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-05-04 15:00:07 -0400

    storing detailed data to location state

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'gatsby'
+import { Octokit } from 'octokit'
 
-export default function ProfileCard({user}) {
-  
+export default function ProfileCard({ user }) {
+
+  // accessing full names from "get a [individual] user" API
+  // not available on index users API
+
+  const [userData, setUserData] = useState({
+    detailedData: [],
+  })
+
+  const octokit = new Octokit({
+    auth: `${process.env.GATSBY_TOKEN}`,
+  })
+
+  useEffect(() => {
+    async function getUser() {
+      const response = await octokit.request(`GET /users/${user.login}`, {
+        username: `${user.login}`,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      })
+      setUserData({
+        detailedData: response.data,
+      })
+    }
+    getUser().catch(console.error)
+  }, [])
+
+  const { detailedData } = userData
+
   return (
-    <div>ProfileCard</div>
+    <div>
+      <h1>{user.login}</h1>
+      <h4>{user.login}</h4>
+      <Link to='/detail' state={{ user: { detailedData } }}>
+        <button>View Profile</button>
+      </Link>
+    </div>
   )
 }
--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import {useLocation} from '@reach/router'
+import DetailCard from '../components/DetailCard'
+import { Link } from 'gatsby'
 
 export default function Detail() {
+
+const location = useLocation()
+
+console.log(location)
+
   return (
     <div>Detail</div>
   )
